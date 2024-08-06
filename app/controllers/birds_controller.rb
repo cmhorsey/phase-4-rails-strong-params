54da0a0e158d57b5438f7db97a3bcb5ded903a43
Cayla Horsey <cayla.horsey@powerhrg.com>
2024-08-06 10:48:12 -0400

    Completed code along

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,18 +1,16 @@
 class BirdsController < ApplicationController
+  wrap_parameters format: []
 
-  # GET /birds
   def index
     birds = Bird.all
     render json: birds
   end
 
-  # POST /birds
   def create
-    bird = Bird.create(name: params[:name], species: params[:species])
+    bird = Bird.create(bird_params)
     render json: bird, status: :created
   end
 
-  # GET /birds/:id
   def show
     bird = Bird.find_by(id: params[:id])
     if bird
@@ -22,4 +20,9 @@ class BirdsController < ApplicationController
     end
   end
 
+  private
+
+  def bird_params
+    params.permit(:name, :species)
+  end
 end
